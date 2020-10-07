1dad61328e13fa4435118cb5573d04702a71900c
Aksel Arzuman <akselarzuman@gmail.com>
2020-10-07 10:53:12 +0300

    delete .vs and .idea folders

--- ArmutLocalStackSample.FunctionalTests/obj/Debug/netcoreapp3.1/ArmutLocalStackSample.FunctionalTests.AssemblyInfo.cs
+++ ArmutLocalStackSample.FunctionalTests/obj/Debug/netcoreapp3.1/ArmutLocalStackSample.FunctionalTests.AssemblyInfo.cs
@@ -11,8 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("ArmutLocalStackSample.Api, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "C:\\Users\\Armut Yedek\\source\\repos\\ArmutLocalStackSample\\ArmutLocalStackSample.Api" +
-    "", "ArmutLocalStackSample.Api.csproj", "0")]
+[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("ArmutLocalStackSample.Api, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "D:\\Github\\FunctionalTestWithLocalStack\\ArmutLocalStackSample.Api", "ArmutLocalStackSample.Api.csproj", "0")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArmutLocalStackSample.FunctionalTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
